0fb2808c18bf167917ddf01852e9da70683b7289
Barsu5489 <ebarsulai@gmail.com>
2025-04-07 09:28:30 +0300

    refactor(session): improve session management in Authentication concern

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -2,6 +2,7 @@ module Authentication
   extend ActiveSupport::Concern
 
   included do
+    before_action :set_current_session
     before_action :require_authentication
     helper_method :authenticated?
   end
@@ -13,40 +14,47 @@ module Authentication
   end
 
   private
+    def set_current_session
+      Rails.logger.debug "Setting current session from cookie: #{cookies.signed[:session_id]}"
+      Current.session = Session.find_by(id: cookies.signed[:session_id])
+      Rails.logger.debug "Current session set to: #{Current.session&.id}"
+    end
+
     def authenticated?
-      resume_session
+      Current.session.present?
     end
 
     def require_authentication
-      resume_session || request_authentication
-    end
-
-    def resume_session
-      Current.session ||= find_session_by_cookie
-    end
-
-    def find_session_by_cookie
-      Session.find_by(id: cookies.signed[:session_id]) if cookies.signed[:session_id]
-    end
-
-    def request_authentication
-      session[:return_to_after_authenticating] = request.url
-      redirect_to new_session_path
-    end
-
-    def after_authentication_url
-      session.delete(:return_to_after_authenticating) || root_url
-    end
-
-    def start_new_session_for(user)
-      user.sessions.create!(user_agent: request.user_agent, ip_address: request.remote_ip).tap do |session|
-        Current.session = session
-        cookies.signed.permanent[:session_id] = { value: session.id, httponly: true, same_site: :lax }
+      unless authenticated?
+        redirect_to login_path, alert: 'Please log in to continue.'
       end
     end
 
+    def start_new_session_for(user)
+      Rails.logger.debug "Creating new session for user: #{user.id}"
+      
+      session = user.sessions.create!(
+        user_agent: request.user_agent,
+        ip_address: request.remote_ip
+      )
+      
+      Current.session = session
+      
+      cookies.signed[:session_id] = {
+        value: session.id,
+        httponly: true,
+        secure: Rails.env.production?,
+        same_site: :lax,
+        expires: 30.days.from_now
+      }
+      
+      Rails.logger.debug "New session created with ID: #{session.id}"
+      session
+    end
+
     def terminate_session
-      Current.session.destroy
+      Current.session&.destroy
       cookies.delete(:session_id)
+      Current.session = nil
     end
 end
