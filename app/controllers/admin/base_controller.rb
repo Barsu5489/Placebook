b9fce4ff86c49ca9d26a087ac4c0960e122523a3
Barsu5489 <ebarsulai@gmail.com>
2025-04-07 20:13:32 +0300

    feat(admin_contoller): - Create Admin::BaseController with admin authentication - Add Admin::UsersController with index and destroy actions - Implement authorization checks for admin routes

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,12 @@
+# app/controllers/admin/base_controller.rb
+module Admin
+    class BaseController < ApplicationController
+      before_action :require_admin
+  
+      private
+  
+      def require_admin
+        redirect_to root_path, alert: "Unauthorized" unless Current.session&.user&.admin?
+      end
+    end
+  end
---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,16 @@
+module Admin
+    class UsersController < BaseController
+      def index
+        @users = User.all
+        render inertia: "Admin/Users/Index", props: {
+          users: @users.as_json(only: [:id, :email_address, :name, :created_at])
+        }
+      end
+  
+      def destroy
+        user = User.find(params[:id])
+        user.destroy
+        redirect_to admin_users_path, notice: "User deleted successfully"
+      end
+    end
+  end
